8ebd515deae62163d4cd5a84a9a8fcde83e07995
Michael N <mnn7135@g.rit.edu>
2023-09-08 00:55:01 -0400

    PW-0002+0003 Prediction Invisible  Sunrise Sunset
    * Now able to view data about sunrise and sunset.
    * Prediction system accounts for sunrise and sunset data.
    * Fix for when no icon displays - data is now filled prior to forecasting.
    * Fix for delay in weather data being displayed due to multiple function runs. Should investigate further.

--- src/server/weather_server.R
+++ src/server/weather_server.R
@@ -13,7 +13,7 @@ source("src/server/weather_server_data.R", local = TRUE)
 # future.
 server <- {
   # Ensure that there is data available from the station.
-  if (!is.na(date_time)) {
+  if (!is.null(pws_data)) {
     # Get the icon and description for the current weather.
     get_current_weather()
     # Outdoor Information
@@ -97,6 +97,19 @@ server <- {
         renderText(sprintf("%.0f\u00B0 F", windchill))
       output$windchill_text <- renderText("Wind Chill")
     }
+    # Display sunrise/sunset information
+    output$sunrise_time <- renderText(format(
+      morning_time,
+      tz = "America/New_York",
+      usetz = TRUE,
+      format = "%I:%M %p"
+    ))
+    output$sunset_time <- renderText(format(
+      evening_time,
+      tz = "America/New_York",
+      usetz = TRUE,
+      format = "%I:%M %p"
+    ))
   } else {
     # Handle if there is no data to pull
     output$weather_tab <- renderText("??\u00B0 F Victor, NY")
--- src/server/weather_server_prediction.R
+++ src/server/weather_server_prediction.R
@@ -4,9 +4,52 @@ source("src/server/weather_server_helper.R", local = TRUE)
 source("src/server/weather_server_data.R", local = TRUE)
 
 server <- {
-  # Current weather icon displayed.
   icon_name <- ""
   icon_desc <- ""
+  # Get the current weather conditions.
+  get_current_weather <- (function() {
+    if (hourly_rain > 0) {
+      icon_name <- icon_rain
+      icon_desc <- weather_rain
+    } else if (wind_speed >= 20) {
+      icon_name <- icon_windy
+      icon_desc <- weather_windy
+    } else {
+      if (morning_time <= current_time && current_time < evening_time) {
+        # Display Daytime Indicators
+        if (uv_index > 3) {
+          icon_name <- icon_sunny
+          icon_desc <- weather_sunny
+        } else {
+          icon_name <- icon_cloudy_sun
+          icon_desc <- weather_cloudy
+        }
+      } else {
+        # Display Nighttime Indicators
+        if ((pws_data[[5]][now_index]
+             - pws_data[[5]][tomorrow_index]) <= -0.20) {
+          icon_name <- icon_cloudy_moon
+          icon_desc <- weather_cloudy
+        } else {
+          icon_name <- icon_clear
+          icon_desc <- weather_clear
+        }
+      }
+    }
+    # Render current weather icons.
+    output$weather_icon <- renderText(as.character(icon(icon_name,
+                                                        size_current)))
+    output$weather_tooltip <-
+      renderText(as.character(icon(icon_name,
+                                   size_tooltip)))
+    output$weather_desc <- renderText(icon_desc)
+    
+    return(c(icon_name, icon_desc))
+  })
+  # Current weather icon displayed.
+  icon_data <- get_current_weather()
+  icon_name <- icon_data[1]
+  icon_desc <- icon_data[2]
   # Predict the weather based on a specific index to compare to as well as
   # a confidence interval.
   predict_weather_tonight <- (function(index, confidence) {
@@ -36,7 +79,10 @@ server <- {
         desc_temp <- weather_cloudy
       }
     } else {
-      if (icon_name == "moon") {
+      if (icon_name == "cloud-moon") {
+        icon_temp <- icon_cloudy_sun
+        desc_temp <- weather_cloudy
+      } else if (icon_name == "moon") {
         icon_temp <- icon_sunny
         desc_temp <- weather_sunny
       } else {
@@ -45,12 +91,12 @@ server <- {
       }
     }
     temp_trend <- 0.66
-    if (current_time < noon_time) {
+    if (morning_time <= current_time && current_time < noon_time) {
       # Likely to warm up a lot in the next 6 hours.
-      temp_trend <- 0.66
+      temp_trend <- -0.66
     } else {
       # Likely to cool down a lot in the next 6 hours.
-      temp_trend <- -0.66
+      temp_trend <- 0.66
     }
     output$weather_icon_later <-
       renderText(as.character(icon(icon_temp, size_predict)))
@@ -92,13 +138,23 @@ server <- {
     } else {
       if (icon_name == "cloud-moon") {
         icon_temp <- icon_cloudy_sun
-        desc_temp <- icon_desc
+        desc_temp <- weather_cloudy
+      } else if (icon_name == "moon") {
+        icon_temp <- icon_sunny
+        desc_temp <- weather_sunny
       } else {
         icon_temp <- icon_name
         desc_temp <- icon_desc
       }
     }
-    temp_trend <- 0.33
+    temp_trend <- 0.66
+    if (morning_time <= current_time && current_time < noon_time) {
+      # Likely to warm up a lot in the next 6 hours.
+      temp_trend <- 0.66
+    } else {
+      # Likely to cool down a lot in the next 6 hours.
+      temp_trend <- -0.66
+    }
     output$weather_icon_1_day <-
       renderText(as.character(icon(icon_temp, size_predict)))
     output$weather_desc_1_day <- renderText(desc_temp)
@@ -109,43 +165,5 @@ server <- {
       )
     )
     output$out_high_1_day <- temp_out
-  })
-  # Get the current weather conditions.
-  get_current_weather <- (function() {
-    if (hourly_rain > 0) {
-      icon_name <- icon_rain
-      icon_desc <- weather_rain
-    } else if (wind_speed >= 20) {
-      icon_name <- icon_windy
-      icon_desc <- weather_windy
-    } else {
-      if (morning_time <= current_time && current_time < evening_time) {
-        # Display Daytime Indicators
-        if (uv_index > 3) {
-          icon_name <- icon_sunny
-          icon_desc <- weather_sunny
-        } else {
-          icon_name <- icon_cloudy_sun
-          icon_desc <- weather_cloudy
-        }
-      } else {
-        # Display Nighttime Indicators
-        if ((pws_data[[5]][now_index]
-             - pws_data[[5]][tomorrow_index]) <= -0.20) {
-          icon_name <- icon_cloudy_moon
-          icon_desc <- weather_cloudy
-        } else {
-          icon_name <- icon_clear
-          icon_desc <- weather_clear
-        }
-      }
-    }
-    # Render current weather icons.
-    output$weather_icon <- renderText(as.character(icon(icon_name,
-                                                        size_current)))
-    output$weather_tooltip <-
-      renderText(as.character(icon(icon_name,
-                                   size_tooltip)))
-    output$weather_desc <- renderText(icon_desc)
   })
 }
